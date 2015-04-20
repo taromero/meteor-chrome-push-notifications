3805e0ffaacaa3517af0897e1b59839d21be481c
Tomas Romero <canotto90@gmail.com>
2015-04-20 20:45:49 -0300

    Remove routing as redirects are not allowed for service workers
    And removed service worker, to let it be defined in the app

--- router.js
+++
@@ -1,8 +0,0 @@
-// we need to serve the service-worker file from the root path, to avoid scope issues
-Router.route('/service-worker.js', function() {
-  this.response.writeHead(302, {
-    'Location': '/packages/canotto90_chrome-push-notifications/service-worker.js'
-  });
-
-  this.response.end();
-}, { where: 'server' })

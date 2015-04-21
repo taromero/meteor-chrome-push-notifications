fb4703404fd7051d49657873bf2499fa662e79bf
Tomas Romero <canotto90@gmail.com>
2015-04-21 00:14:50 -0300

    Optionally check for password when asking to broadcast

--- methods.js
+++ methods.js
@@ -8,7 +8,12 @@ Meteor.methods({
   remove_subscription: function(subscriptionId) {
     return PnSubscriptions.remove({ subscription_id: subscriptionId })
   },
-  broadcast_push_notifications: function() {
+  broadcast_push_notifications: function(password) {
+    if (Meteor.settings.push_notifications_password) {
+      if (password != Meteor.settings.push_notifications_password) {
+        throw new Meteor.Error(401, 'Incorrect password to send push notification broadcast')
+      }
+    }
     subscription_ids = PnSubscriptions.find().map(getSubscriptionIds)
     HTTP.post('https://android.googleapis.com/gcm/send', {
       headers: {
